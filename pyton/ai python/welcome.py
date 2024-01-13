55dce53cbabd84eed9604e282c5f12e7ff2e726e
Mohamed Ali <145205028+MBG5@users.noreply.github.com>
2024-01-13 13:59:43 +0000

    Add files via upload

---
+++ pyton/ai python/welcome.py
@@ -0,0 +1,11 @@
+line01 ="*******************"
+line02 ="*    WELCOME      *"
+line03 ="*                 *"
+
+
+print(line01)
+print(line03)
+print(line02)
+print(line03)
+print(line01)
+
---
+++ pyton/lesson 4/operator.py
@@ -0,0 +1,12 @@
+meaning = 42 
+print('')
+
+if meaning > 10:
+    print('right on!')
+else:
+    print('Not Today')
+
+#tenary operator
+print('right on!') if meaning > 10 else print('Not today')
+
+
